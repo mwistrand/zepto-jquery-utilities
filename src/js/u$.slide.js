6786618a0b3b2a94ac8d9caa0834224e670b3ea7
Matt Wistrand <matt@troglodytidae.com>
2013-12-17 14:03:23 -0600

    Added documentation for u$.slide.js. $.fn.slideUp and $.fn.slideDown now return 'this'

--- src/js/u$.slide.js
+++ src/js/u$.slide.js
@@ -1,23 +1,34 @@
+/**
+ * Provides functionality for sliding up or down DOM elements. The following
+ * methods are included:
+ * `u$.slideUp`
+ * `u$.slideDown`
+ * `$.fn.slideUp` for Zepto
+ * `$.fn.slideDown` for Zepto
+ */
 (function($) {
 'use strict';
 
 window.u$ || (window.u$ = {});
 
 var toZero = {
-  paddingTop: 0,
-  paddingBottom: 0,
-  marginTop: 0,
-  marginBottom: 0
-},
-boxStyles = $.extend(Object.create(toZero), {
-  height: 0
-}),
-initial = {
-  display: 'block',
-  height: 0,
-  overflow: 'hidden'
-};
+    paddingTop: 0,
+    paddingBottom: 0,
+    marginTop: 0,
+    marginBottom: 0
+  },
+  boxStyles = $.extend(Object.create(toZero), {
+    height: 0
+  }),
+  initial = {
+    display: 'block',
+    height: 0,
+    overflow: 'hidden'
+  };
 
+// jQuery's `$.fn.slideUp` and `$.fn.slideDown` can take either an
+// object of options, or certain options can be provided directly
+// as arguments. This function converts an arguments list to an object.
 function setOptions() {
   var options = {
     duration: arguments[0]
@@ -33,6 +44,8 @@ function setOptions() {
   return options;
 }
 
+// Decorates an options object's `complete` method with a method
+// that will be called before the original function.
 function extendOpts($el, options, fn) {
   var complete = options && options.complete;
 
@@ -47,6 +60,9 @@ function extendOpts($el, options, fn) {
   });
 }
 
+// Returns an object containing an element's `overflowX` and `overflowY`.
+// properties. Unless the element's `overflowY` value is `scroll`, the
+// `overflowY` value of the returned object will always be `hidden`.
 function getOverflow($el) {
   var css = {
     overflowY: $el.css('overflow-y'),
@@ -60,6 +76,10 @@ function getOverflow($el) {
   return css;
 }
 
+// Slides the element up to a specified height, or to 0. If a height
+// is specified, then only the `height` is animated. Otherwise, the
+// `height`, `padding-top`, `padding-bottom`, `margin-top`, and
+// `margin-bottom` values are animated.
 function slideUp($el, height, options) {
   var props = !height ? boxStyles : { height: height },
     old = u$.getStyles($el, props),
@@ -74,6 +94,10 @@ function slideUp($el, height, options) {
   $el.css(css).animate(props, opts);
 }
 
+// Slides the element down to a specified height, or to its full height.
+// If at least a portion of the element is currently visible, then only
+// the `height` will be animated. Otherwise, the `height`, `padding-top`,
+// `padding-bottom`, `margin-top`, and `margin-bottom` values are animated.
 function slideDown($el, height, options) {
   var props = !$el.get(0).offsetHeight ? boxStyles : {},
     toHeight = height || u$.height($el),
@@ -85,12 +109,29 @@ function slideDown($el, height, options) {
   $el.css(css).animate(toAnimate, options);
 }
 
+/**
+ * Slides an element up to a specified height, or 0.
+ *
+ * @param $el The `$` element to animate
+ * @param height The optional height for the animation. Defaults to 0.
+ * @param options An object of options controlling the animation. These
+ *                are identical to the options for jQuery's `$.fn.slideUp.`
+ */
 u$.slideUp = function($el, height, options) {
   height = u$.toInt(height) ? height : 0;
 
   slideUp($el, height, options);
 };
 
+/**
+ * Slides an element down to a specified height, or to its full height.
+ *
+ * @param $el The `$` element to animate
+ * @param height The optional height for the animation. Defaults to the
+ *               element's full height.
+ * @param options An object of options controlling the animation. These
+ *                are identical to the options for jQuery's `$.fn.slideDown.`
+ */
 u$.slideDown = function($el, height, options) {
   height = u$.toInt(height) ? height : 0;
 
@@ -98,22 +139,32 @@ u$.slideDown = function($el, height, options) {
 };
 
 if (!$.fn.slideUp) {
+  /**
+   * This is included for Zepto, and is identical to the jQuery equivalent.
+   */
   $.fn.slideUp = function(options) {
     if (!$.isPlainObject(options)) {
       options = setOptions.apply(null, arguments);
     }
 
     slideUp(this, 0, options);
+
+    return this;
   };
 }
 
 if (!$.fn.slideDown) {
+  /**
+   * This is included for Zepto, and is identical to the jQuery equivalent.
+   */
   $.fn.slideDown = function(options) {
     if (!$.isPlainObject(options)) {
       options = setOptions.apply(null, arguments);
     }
 
     slideDown(this, 0, options);
+
+    return this;
   };
 }
 

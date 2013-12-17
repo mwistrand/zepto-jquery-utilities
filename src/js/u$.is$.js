977698fde30df153344fe665af5397d7b7a16590
Matt Wistrand <matt@troglodytidae.com>
2013-12-17 14:13:25 -0600

    Added documentation for u$.is$.js

--- src/js/u$.is$.js
+++ src/js/u$.is$.js
@@ -3,6 +3,13 @@
 
 window.u$ || (window.u$ = {});
 
+/**
+ * Determines whether an object is an instance of `$`.
+ *
+ * @param obj The object to test.
+ *
+ * @returns Boolean true if the test passed or false.
+ */
 u$.is$ = function(obj) {
   return (window.Zepto) ? $.zepto.isZ(obj) : (obj instanceof $);
 };

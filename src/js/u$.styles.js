2b1c3dbc3fc7268146d3ae923ba098e2aea2e8b1
Matt Wistrand <matt@troglodytidae.com>
2013-12-17 09:32:41 -0600

    Removed u$.getNumericStyles

--- src/js/u$.styles.js
+++ src/js/u$.styles.js
@@ -9,42 +9,6 @@ var unhideProps = {
   visibility: 'hidden'
 };
 
-u$.getNumericStyles = (function() {
-  var directions = 'top,right,bottom,left'.split(',');
-  
-  function testStyle(style) {
-    return !isNaN(parseInt(style, 10));
-  }
-  
-  function setStyle($el, name, styles) {
-    var style = $el.css(name);
-    
-    if (testStyle(style)) {
-      styles[name] = style;
-    }
-    
-    return style;
-  }
-  
-  return function($el, styles) {
-    var props = [].slice.call(arguments, 2);
-    
-    $.each(props, function(i, name) {
-      var style = $el.css(name + '-top');
-
-      if (style !== null) {
-        $.each(directions, function(i, dir) {
-          setStyle($el, name + '-' + dir, styles);
-        });
-      } else {
-        setStyle($el, name, styles);
-      }
-    });
-    
-    return styles;
-  };
-})();
-
 u$.getStyles = function($el, props, callback) {
   var old = {};
   
